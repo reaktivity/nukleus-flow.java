4d970f29e1827e2f0388ad0a4aceac8114f964a1
John Fallows <john.fallows@kaazing.com>
2019-02-28 11:43:00 -0800

    Initial implementation

---
+++ src/main/java/org/reaktivity/nukleus/flow/internal/stream/FlowProxyFactoryBuilder.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright 2016-2018 The Reaktivity Project
+ *
+ * The Reaktivity Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.reaktivity.nukleus.flow.internal.stream;
+
+import java.util.function.IntUnaryOperator;
+import java.util.function.LongFunction;
+import java.util.function.LongSupplier;
+import java.util.function.LongUnaryOperator;
+import java.util.function.Supplier;
+
+import org.agrona.MutableDirectBuffer;
+import org.reaktivity.nukleus.buffer.BufferPool;
+import org.reaktivity.nukleus.flow.internal.FlowConfiguration;
+import org.reaktivity.nukleus.route.RouteManager;
+import org.reaktivity.nukleus.stream.StreamFactory;
+import org.reaktivity.nukleus.stream.StreamFactoryBuilder;
+
+public final class FlowProxyFactoryBuilder implements StreamFactoryBuilder
+{
+    private final FlowConfiguration config;
+
+    private RouteManager router;
+    private MutableDirectBuffer writeBuffer;
+    private LongUnaryOperator supplyInitialId;
+    private LongUnaryOperator supplyReplyId;
+    private LongSupplier supplyTraceId;
+    private LongSupplier supplyCorrelationId;
+
+    public FlowProxyFactoryBuilder(
+        FlowConfiguration config)
+    {
+        this.config = config;
+    }
+
+    @Override
+    public FlowProxyFactoryBuilder setRouteManager(
+        RouteManager router)
+    {
+        this.router = router;
+        return this;
+    }
+
+    @Override
+    public FlowProxyFactoryBuilder setWriteBuffer(
+        MutableDirectBuffer writeBuffer)
+    {
+        this.writeBuffer = writeBuffer;
+        return this;
+    }
+
+    @Override
+    public StreamFactoryBuilder setInitialIdSupplier(
+        LongUnaryOperator supplyInitialId)
+    {
+        this.supplyInitialId = supplyInitialId;
+        return this;
+    }
+
+    @Override
+    public StreamFactoryBuilder setReplyIdSupplier(
+        LongUnaryOperator supplyReplyId)
+    {
+        this.supplyReplyId = supplyReplyId;
+        return this;
+    }
+
+    @Override
+    public StreamFactoryBuilder setTraceSupplier(
+        LongSupplier supplyTraceId)
+    {
+        this.supplyTraceId = supplyTraceId;
+        return this;
+    }
+
+    @Override
+    public StreamFactoryBuilder setTargetCorrelationIdSupplier(
+        LongSupplier supplyCorrelationId)
+    {
+        this.supplyCorrelationId = supplyCorrelationId;
+        return this;
+    }
+
+    @Override
+    public FlowProxyFactoryBuilder setGroupBudgetClaimer(
+        LongFunction<IntUnaryOperator> groupBudgetClaimer)
+    {
+        return this;
+    }
+
+    @Override
+    public FlowProxyFactoryBuilder setGroupBudgetReleaser(
+        LongFunction<IntUnaryOperator> groupBudgetReleaser)
+    {
+        return this;
+    }
+
+    @Override
+    public StreamFactoryBuilder setBufferPoolSupplier(
+        Supplier<BufferPool> supplyBufferPool)
+    {
+        return this;
+    }
+
+    @Override
+    public StreamFactory build()
+    {
+        return new FlowProxyFactory(
+                config,
+                router,
+                writeBuffer,
+                supplyInitialId,
+                supplyReplyId,
+                supplyTraceId,
+                supplyCorrelationId);
+    }
+}
